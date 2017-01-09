f5f98abb55b92a308e6b0ec625c45ab1adf38e72
primoz klemensek <klemensek@gmail.com>
2017-01-09 19:05:54 +0100

    final question parser

--- parse_questions.php
+++ parse_questions.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'vendor/autoload.php';
+//require 'vendor/autoload.php';
 include_once('inc/config.php');
 
 error_reporting(E_ERROR | E_WARNING);
@@ -24,7 +24,7 @@ http://www.dz-rs.si/wps/portal/Home/deloDZ/poslanskaVprasanjaInPobude/
 
 
 
-function parseQuestionStart($urls)
+function parseQuestionStart($urls, $startPage, $series)
 {
     global $http_response_header, $pobuda;
 
@@ -41,7 +41,6 @@ function parseQuestionStart($urls)
                     $cookiess .= $parts[1] . '=' . $parts[2] . '; ';
             }
         }
-
         $cookiess = substr($cookiess, 0, -2);
 
 
@@ -53,11 +52,12 @@ function parseQuestionStart($urls)
         preg_match('/Page 1 of (\d+)/i', $base, $matchesp);
 
         if (!empty($matchesp[1]) && (int)$matchesp[1] > 0) {
-            for ($i = 1; $i <= (int)$matchesp[1]; $i++) {
+            $i = $startPage;
+            //for ($i = $startPage; $i <= (int)$matchesp[1]; $i++) {
                 $postdata = http_build_query(
                     array(
                         //$form_id . ':form1' => $form_id . ':form1',
-                        $form_id . ':form1:menu1' => CURRENT_SESSION,
+                        //$form_id . ':form1:menu1' => CURRENT_SESSION,
 
                         $form_id . ':form1:vprPobude1:goto1__pagerGoButton.x' => 1,
                         $form_id . ':form1:vprPobude1:goto1__pagerGoText' => $i,
@@ -68,32 +68,35 @@ function parseQuestionStart($urls)
                         $form_id . ':form1:naslovljenecFilter:' => '',
                         $form_id . ':form1:selSeja:' => '',
                         $form_id . ':form1_SUBMIT' => 1,
-                        'javax.faces.ViewState' => $matchess[1],
-                        'javax.faces.encodedURL' => '/wps/PA_DZ-LN-VnjaInPobude/portlet/VprasanjaInPobudeView.jsp'
+                        'javax.faces.ViewState' => $matchess[1]
+                        ,'javax.faces.encodedURL' => '/wps/PA_DZ-LN-VnjaInPobude/portlet/VprasanjaInPobudeView.jsp'
                     )
                 );
                 $opts = array('http' =>
                     array(
                         'method' => 'POST',
                         'header' => 'Cookie: ' . $cookiess . "\r\n" . 'Content-type: application/x-www-form-urlencoded',
+                        'timeout' => 2,
                         'content' => $postdata
                     )
                 );
 
                 $context = stream_context_create($opts);
-                if ($subpageContent = file_get_contents(DZ_URL . $matches[1] . '#Z7_KIOS9B1A080280I1UOFDUG3081', false, $context)) {
-                    parseQuestionList($subpageContent, $i, $cookiess);
+                if ($subpageContent = downloadPage(DZ_URL . $matches[1] . '#Z7_KIOS9B1A080280I1UOFDUG3081', $context)) {
+
+
+                    parseQuestionList($subpageContent, $i, $cookiess, $series);
                 }
 
-                var_dump($i);
-
-            }
+                var_dump("page: " . $i ." out of: ".(int)$matchesp[1]);
+return false;
+            //}
         }
     }
 }
 
 
-function parseQuestionList($content, $page, $cookiess)
+function parseQuestionList($content, $page, $cookiess, $series)
 {
 
     global $http_response_header, $pobuda, $pobudaKey;
@@ -108,7 +111,31 @@ function parseQuestionList($content, $page, $cookiess)
         return false;
     }
 
+    $listPager = 1;
     foreach ($data as $link) {
+
+        if($series == 1){
+            if($listPager == 6){
+                break;
+            }
+        }
+        if($series == 2){
+            if($listPager < 6){
+                $listPager++;
+                continue;
+            }
+            if($listPager == 11){
+                break;
+            }
+        }
+        if($series == 3){
+            if($listPager < 11){
+                $listPager++;
+                continue;
+            }
+
+        }
+
 
         $innerData = $link->find('td');
 
@@ -178,13 +205,25 @@ function parseQuestionList($content, $page, $cookiess)
         $context = stream_context_create($opts);
 
 
-        if ($subpageContent = file_get_contents(DZ_URL . $matches[1] . '#Z7_KIOS9B1A080280I1UOFDUG3081', false, $context)) {
+        if ($subpageContent = downloadPage(DZ_URL . $matches[1] . '#Z7_KIOS9B1A080280I1UOFDUG3081', $context)) {
+
+//            $cookiess = '';
+//            if (isset($http_response_header)) {
+//                foreach ($http_response_header as $s) {
+//                    if (preg_match('|^Set-Cookie:\s*([^=]+)=([^;]+);(.+)$|', $s, $parts))
+//                        $cookiess .= $parts[1] . '=' . $parts[2] . '; ';
+//                }
+//            }
+//            $cookiess = substr($cookiess, 0, -2);
+//            var_dump($cookiess);
+
             parseQuestionData($subpageContent, $cookiess);
         }
 
         $data = $pobuda[$pobudaKey];
         sendDataToKunst($pobuda, $pobudaKey, $data);
         $pobudaKey++;
+        $listPager++;
     }
 
 }
@@ -237,7 +276,7 @@ function parseQuestionData($content, $cookiess)
             array(
                 //$form_id . ':form1' => $form_id . ':form1',
 
-                $form_id . ':form1:selMandati:' => 'ZP\\\\POS_VPR.NSF',
+                $form_id . ':form1:selMandati:' => 'ZP\\POS_VPR.NSF',
                 $form_id . ':form1:selAvtorji:' => '',
                 $form_id . ':form1:nazivFilter:' => '',
                 $form_id . ':form1:naslovljenecFilter:' => '',
@@ -262,7 +301,17 @@ function parseQuestionData($content, $cookiess)
         );
         $context = stream_context_create($opts);
 
-        if ($subpage = file_get_contents(DZ_URL . $matches[1] . '#Z7_KIOS9B1A080280I1UOFDUG3081', false, $context)) {
+        if ($subpage = downloadPage(DZ_URL . $matches[1] . '#Z7_KIOS9B1A080280I1UOFDUG3081', $context)) {
+
+//            $cookiess = '';
+//            if (isset($http_response_header)) {
+//                foreach ($http_response_header as $s) {
+//                    if (preg_match('|^Set-Cookie:\s*([^=]+)=([^;]+);(.+)$|', $s, $parts))
+//                        $cookiess .= $parts[1] . '=' . $parts[2] . '; ';
+//                }
+//            }
+//            $cookiess = substr($cookiess, 0, -2);
+//            var_dump($cookiess);
 
             $subData = str_get_html($subpage);
 
@@ -316,25 +365,24 @@ function parseData($data, $cookiess)
 
 function sendDataToKunst($pobuda, $pobudaKey, $data){
 
-    file_put_contents("log/first.txt", print_r($data, true));
+    file_put_contents("log/first.txt", print_r($data, true), FILE_APPEND);
 
 
-    $url = 'https://data.parlameter.si/v1/addQuestion/';
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($pobuda[$pobudaKey]));
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, ($pobuda[$pobudaKey]));
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-
-    $result = curl_exec($ch);
-    curl_close($ch);
+//    $url = 'https://data.parlameter.si/v1/addQuestion/';
+//
+//    $ch = curl_init();
+//    curl_setopt($ch, CURLOPT_URL, $url);
+//    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//    //curl_setopt($ch, CURLOPT_POST, 1);
+//    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+//    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+//    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data, JSON_UNESCAPED_UNICODE));
+//
+//    $result = curl_exec($ch);
+//    curl_close($ch);
+//
+//    file_put_contents("log/first.txt", print_r($result, true), FILE_APPEND);
 
     var_dump($pobudaKey);
 
@@ -360,38 +408,52 @@ function unichr($dec) {
 
 function translateCharacters($in){
 
-    $a = htmlentities($in);
-
-    $b = html_entity_decode($a);
-
-    $b = preg_replace("/&#(\d{2,5});/e", "unichr($1);", $b);
+    mb_internal_encoding('utf-8');
+    $a = mapperSumniki($in);
+    $b = html_entity_decode($a, ENT_COMPAT, 'UTF-8');
 
     return $b;
 }
 
 function translatePS($in){
-$search = array();
-$search[] = 'Poslanska skupina Slovenske demokratske stranke';
-$search[] = 'Poslanska skupina Stranke modernega centra';
-$search[] = 'Poslanska skupina Združena levica';
-$search[] = 'AČ - Nepovezani poslanec';
-$search[] = 'Poslanska skupina Demokratične stranke upokojencev Slovenije';
-$search[] = 'Poslanska skupina nepovezanih poslancev';
-$replace = array();
-$replace[] = 'PS Slovenska Demokratska Stranka';
-$replace[] = 'PS Stranka modernega centra';
-$replace[] = 'PS Združena Levica';
-$replace[] = 'Nepovezani poslanec Andrej Čuš';
-$replace[] = 'PS Demokratska Stranka Upokojencev Slovenije';
-$replace[] = 'PS nepovezanih poslancev ';
 
-if(!in_array($in, $search)){
-    return $in;
-}
+    $search = array();
+    $search[] = 'Poslanska skupina Slovenske demokratske stranke';
+    $search[] = 'Poslanska skupina Stranke modernega centra';
+    $search[] = 'Poslanska skupina Združena levica';
+    $search[] = 'AČ - Nepovezani poslanec';
+    $search[] = 'Poslanska skupina Demokratične stranke upokojencev Slovenije';
+    $search[] = 'Poslanska skupina nepovezanih poslancev';
+
+    $replace = array();
+    $replace[] = 'PS Slovenska Demokratska Stranka';
+    $replace[] = 'PS Stranka modernega centra';
+    $replace[] = 'PS Združena Levica';
+    $replace[] = 'Nepovezani poslanec Andrej Čuš';
+    $replace[] = 'PS Demokratska Stranka Upokojencev Slovenije';
+    $replace[] = 'PS nepovezanih poslancev ';
+
+    if(!in_array($in, $search)){
+        return $in;
+    }
 
     return str_replace($search, $replace, $in);
 
 }
 
+function mapperSumniki($in){
+    $search = array('&#160;');
+    $replace = array(' ');
 
-parseQuestionStart(array('http://www.dz-rs.si/wps/portal/Home/deloDZ/poslanskaVprasanjaInPobude/'));
+    return str_replace($search, $replace, $in);
+
+}
+
+for ($i = 1; $i<35; $i++){
+    parseQuestionStart(array('http://www.dz-rs.si/wps/portal/Home/deloDZ/poslanskaVprasanjaInPobude/'), $i, 1);
+    parseQuestionStart(array('http://www.dz-rs.si/wps/portal/Home/deloDZ/poslanskaVprasanjaInPobude/'), $i, 2);
+    parseQuestionStart(array('http://www.dz-rs.si/wps/portal/Home/deloDZ/poslanskaVprasanjaInPobude/'), $i, 3);
+    die();
+}
+
+
