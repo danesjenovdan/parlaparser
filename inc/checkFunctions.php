bc3e301790fe31b18e8d3218da85abc00e667ed7
klemensek <klemensek@gmail.com>
2017-01-31 21:36:51 +0100

    additiona fixes for documents

--- inc/checkFunctions.php
+++ inc/checkFunctions.php
@@ -77,6 +77,7 @@ function prepareDataSharedSession($url)
 
 }
 
+
 function motionExists($session_id, $organization_id, $date, $name)
 {
 
@@ -101,4 +102,66 @@ function motionExists($session_id, $organization_id, $date, $name)
     }
     return false;
 
+}
+
+/*
+ * //DROP TABLE parladata_tmp_votelinks;
+CREATE TABLE parladata_tmp_votelinks
+(
+    id INTEGER PRIMARY KEY NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    session_id VARCHAR(255),
+    gov_id VARCHAR(255),
+    votedoc_url VARCHAR(255)
+);
+CREATE SEQUENCE parladata_tmp_votelinks_id_seq NO MINVALUE NO MAXVALUE NO CYCLE;
+ALTER TABLE parladata_tmp_votelinks ALTER COLUMN id SET DEFAULT nextval('parladata_tmp_votelinks_id_seq');
+ALTER SEQUENCE parladata_tmp_votelinks_id_seq OWNED BY parladata_tmp_votelinks.id;
+GRANT ALL PRIVILEGES ON TABLE parladata_tmp_votelinks TO parladaddy;
+
+
+ */
+
+function voteLinkExists($session_id, $gov_id, $url)
+{
+    global $conn;
+
+    $sql = "
+    select * from parladata_tmp_votelinks
+    where 
+		session_id = '" . $session_id . "' AND
+		gov_id = '" . pg_escape_string ($conn, $gov_id) . "' AND
+		votedoc_url = '" . pg_escape_string ($conn, $url) . "'
+		;
+    ";
+
+    $result = pg_query ($conn, $sql);
+    if ($result) {
+        if (pg_num_rows ($result) > 0) {
+            return true;
+        }
+    }
+    return false;
+}
+function voteLinkInsert($session_id, $gov_id, $url)
+{
+    global $conn;
+
+    $sql = "
+			INSERT INTO
+				parladata_tmp_votelinks
+			(created_at, updated_at, session_id, gov_id, votedoc_url)
+			VALUES
+			(NOW(), NOW(), '" . $session_id . "', '" . pg_escape_string ($conn, $gov_id) . "', '" . pg_escape_string ($conn, $url) . "')
+			
+    ";
+
+    $voting_id = 0;
+    $result = pg_query ($conn, $sql);
+    if (pg_affected_rows ($result) > 0) {
+        $insert_row = pg_fetch_row($result);
+        $voting_id = $insert_row[0];
+    }
+    return $voting_id;
 }
