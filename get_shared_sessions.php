4b9454b1f99d11023f6b700bd185ff4d8254d0f3
klemensek <klemensek@gmail.com>
2017-02-09 10:31:47 +0100

    prepared fix for shared sesions

--- get_shared_sessions.php
+++ get_shared_sessions.php
@@ -8,10 +8,6 @@ require 'vendor/autoload.php';
 include_once('inc/config.php');
 
 
-$sharedSessions = checkSharedSessions(date("Ymd"));
-//$sharedSessions = unserialize(file_get_contents("gitignore/sharedSessions".date("Ymd")));
-
-
 function array_sort_by_column(&$arr, $col, $dir = SORT_ASC) {
     $sort_col = array();
     foreach ($arr as $key=> $row) {
@@ -21,61 +17,64 @@ function array_sort_by_column(&$arr, $col, $dir = SORT_ASC) {
     array_multisort($sort_col, $dir, $arr);
 }
 
+function insertMissingSharedSessions($sharedSessions){
+    array_sort_by_column($sharedSessions, 'sharedSessionKey');
 
-array_sort_by_column($sharedSessions, 'sharedSessionKey');
+    $similar = array();
+    foreach ($sharedSessions as $item) {
+        //$key = md5($item['sharedSessionKey']);
+        $key = $item['sharedSessionKey'];
 
-$similar = array();
-foreach ($sharedSessions as $item) {
-    $key = md5($item['sharedSessionKey']);
-    $key = $item['sharedSessionKey'];
+        print $key . " " . $item["sessionId"] ."\n";
 
-    print $key . " " . $item["sessionId"] ."\n";
-
-    if(!array_key_exists($key, $similar)){
-        $similar[$key]["st"] = 1;
-        $similar[$key]["sessionIds"] = $item["sessionId"];
-        $similar[$key]["organizationId"] = $item["organizationId"];
-    }else{
-        $similar[$key]["st"] = $similar[$key]["st"] + 1;
-        $similar[$key]["sessionIds"] = $similar[$key]["sessionIds"] .','. $item["sessionId"];
-        $similar[$key]["organizationId"] = $similar[$key]["organizationId"] .','. $item["organizationId"];
-    }
-}
-
-$i = 1;
-foreach ($similar as $item) {
-    if($item["st"] > 1){
-
-        var_dump($item["sessionIds"]);
-        var_dump($item["organizationId"]);
-        $sessionIds = explode(",", $item["sessionIds"]);
-        $sessionId = $sessionIds[0];
-        $orgIds = explode(",", $item["organizationId"]);
-        foreach ($orgIds as $orgId) {
-            insertToSessionOrganizations($sessionId, $orgId);
+        if(!array_key_exists($key, $similar)){
+            $similar[$key]["st"] = 1;
+            $similar[$key]["sessionIds"] = $item["sessionId"];
+            $similar[$key]["organizationId"] = $item["organizationId"];
+        }else{
+            $similar[$key]["st"] = $similar[$key]["st"] + 1;
+            $similar[$key]["sessionIds"] = $similar[$key]["sessionIds"] .','. $item["sessionId"];
+            $similar[$key]["organizationId"] = $similar[$key]["organizationId"] .','. $item["organizationId"];
         }
+    }
 
-        $sessionIdsDelete = array_shift($sessionIds);
+    $i = 1;
+    foreach ($similar as $item) {
+        if($item["st"] > 1){
 
-        foreach ($sessionIds as $sessionId) {
-            if(sessionDeletedById($sessionId)){
-                continue;
+            var_dump($item["sessionIds"]);
+            var_dump($item["organizationId"]);
+            $sessionIds = explode(",", $item["sessionIds"]);
+            $sessionId = $sessionIds[0];
+            $orgIds = explode(",", $item["organizationId"]);
+            foreach ($orgIds as $orgId) {
+                insertToSessionOrganizations($sessionId, $orgId);
             }
-            makeSessionBackupInDeleted($sessionId);
-            deleteSessionRelation($sessionId);
+
+            $sessionIdsDelete = array_shift($sessionIds);
+
+            foreach ($sessionIds as $sessionId) {
+                if(sessionDeletedById($sessionId)){
+                    continue;
+                }
+                makeSessionBackupInDeleted($sessionId);
+                deleteSessionRelation($sessionId);
+            }
+
+
+            $i++;
+        }else{
+
+            $sessionIds = explode(",", $item["sessionIds"]);
+
+            print($sessionIds[0])."\n";
         }
 
-
-        $i++;
-    }else{
-
-        $sessionIds = explode(",", $item["sessionIds"]);
-
-        print($sessionIds[0])."\n";
     }
-
+    var_dump($i);
 }
-var_dump($i);
 
 
-die();
+$sharedSessions = getMissingSharedSessions(date("Ymd"));
+//$sharedSessions = unserialize(file_get_contents("gitignore/sharedSessions".date("Ymd")));
+insertMissingSharedSessions($sharedSessions);
