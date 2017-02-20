fddb6b9b8e52f272df182896bed5b061131ccdc8
klemensek <klemensek@gmail.com>
2017-02-20 21:28:48 +0100

    partial solution for update

--- parseLast.php
+++ parseLast.php
@@ -9,20 +9,25 @@ include_once('inc/config_custom.php');
 
 function importerUsage(){
     die("<<<USAGE
-Usage:  php parseLast.php what=docs limit=2 organization_id=95 parse_speeches_force=true  > /dev/null &
+Usage:  php parseLast.php limit=2 organization_id=95 PARSE_SPEECHES=true PARSE_SPEECHES_FORCE=true PARSE_VOTES=true PARSE_VOTES_DOCS=true PARSE_DOCS=true > /dev/null &
+
+php parseLast.php limit=2 organization_id=95 PARSE_SPEECHES=true PARSE_SPEECHES_FORCE=true PARSE_VOTES=true PARSE_VOTES_DOCS=true PARSE_DOCS=true > /log/parseLastLog_20170217
  
-what - speeches / docs / votes
 limit - numeber of last sessions
 organization_id - if 0, no filter, 95 for DZ
 
-parse_speeches_force - default false
+PARSE_SPEECHES - default false
+PARSE_SPEECHES_FORCE - default false
+PARSE_VOTES - default false
+PARSE_VOTES_DOCS - default false
+PARSE_DOCS - default false
  
 USAGE;");
 }
 
 if (count($argv) == 1) importerUsage();
 
-$obligatoryFields = array('what', 'limit', 'organization_id', 'parse_speeches_force');
+$obligatoryFields = array('limit', 'organization_id', 'PARSE_SPEECHES', 'PARSE_SPEECHES_FORCE', 'PARSE_VOTES', 'PARSE_VOTES_DOCS', 'PARSE_DOCS');
 
 $argvCount = 0;
 $sessionCustomOptions = array();
@@ -48,15 +53,18 @@ if(count($obligatoryFields) != $argvCount){
 }
 
 
-define('PARSE_SPEECHES', ($sessionCustomOptions['what'] == 'speeches') ? true : false);
-define('PARSE_SPEECHES_FORCE', ($sessionCustomOptions['parse_speeches_force'] == 'true') ? true : false);
-define('PARSE_VOTES', ($sessionCustomOptions['what'] == 'votes') ? true : false);
-define('PARSE_DOCS', ($sessionCustomOptions['what'] == 'docs') ? true : false);
+define('PARSE_SPEECHES', ($sessionCustomOptions['PARSE_SPEECHES'] == 'true') ? true : false);
+define('PARSE_SPEECHES_FORCE', ($sessionCustomOptions['PARSE_SPEECHES_FORCE'] == 'true') ? true : false);
+define('PARSE_VOTES', ($sessionCustomOptions['PARSE_VOTES'] == 'true') ? true : false);
+define('PARSE_VOTES_DOCS', ($sessionCustomOptions['PARSE_VOTES_DOCS'] == 'true') ? true : false);
+define('PARSE_DOCS', ($sessionCustomOptions['PARSE_DOCS'] == 'true') ? true : false);
 
 define('SKIP_WHEN_REVIEWS', false);
 define('UPDATE_SESSIONS_IN_REVIEW', true);
 
 define('FORCE_UPDATE', true);
+
+
 
 
 // Get people array
@@ -71,7 +79,7 @@ foreach ($sessions as $session) {
     if (isset($session['gov_id'])) {
 var_dump("session Id " . $session["id"]);
         $content = file_get_contents('http://www.dz-rs.si' . htmlspecialchars_decode($session['gov_id']));
-        parseSessionsSingle($content, $session['organization_id'], $session);
+        parseSessionsSingleUpdate($content, $session['organization_id'], $session);
 
     }
 
